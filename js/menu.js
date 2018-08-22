2f0b95f3322ec4787ceed8800131d6b159db4ad1
Bruno Silva <bruno.ti.es@gmail.com>
2018-08-22 01:09:41 -0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,16 @@
-document.querySelector('.menu-controle').onclick = function() {
-	document.documentElement.classList.toggle('menu-ativo');
+$(function () {
+  $('.menu-abrir').click(exibeMenu);
+  $('.menu-fechar').click(ocultaMenu);
+});
+function exibeMenu() {
+  $('html').addClass('menu-ativo');
+}
+function ocultaMenu() {
+  $('html').removeClass('menu-ativo');
+}
+
+document.documentElement.onclick = function(event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu-ativo');
+    }
 };
